000fbbefb98e757bc3e027c9883e088dec23b96a
Sumit Jain <jainmit23@gmail.com>
2023-01-20 16:39:20 +0530

    fix:type

--- dashboard/src/db.ts
+++ dashboard/src/db.ts
@@ -1,9 +1,18 @@
 import Dexie from 'dexie';
 
-export const db = new Dexie('myAssetBuddy');
+// export const db = new Dexie('myAssetBuddy');
 // db.version(1).stores({
 //   docs: '_id, name, doctype, lastFetchedOn,modified,count', // Primary key and indexed props
 // });
+
+// export interface Doc {
+//   _id: string;
+//   name: string;
+//   doctype: string;
+//   lastFetchedOn: Date;
+//   modified: string;
+//   count: number;
+// }
 
 export const DexieDatabase = (databaseName: string = 'frappe-react-indexdb', version: number = 1) => {
   const db = new Dexie(databaseName);
